f099b8fbb6ad2feeb8184f317c05f887f55f7617
rezone <rezoneprinceme@gmail.com>
2023-12-10 18:22:26 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const categoris = require("./data/categoris.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 
@@ -13,6 +14,26 @@ app.get("/", (req, res) => {
 
 app.get("/categoris", (req, res) => {
   res.send(categoris);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedId = news.find((n) => n._id === id);
+  res.send(selectedId);
+});
+
+app.get("/categoris/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+  }
 });
 
 app.listen(port, () => {
